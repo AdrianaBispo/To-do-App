811a6e7f1ed424931c8e83b524a871b4a5879ee9
dev <adriana.bispo283@gmail.com>
2022-11-05 21:23:42 -0300

    acriado metodo add todo no FirebaseCrud

--- lib/src/services/firebase_crud.dart
+++ lib/src/services/firebase_crud.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 //models
 import '../models/response.dart';
+import '../models/tasks.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final CollectionReference _collection = _firestore.collection('Tasks');
@@ -12,6 +13,25 @@ class FirebaseCrud {
     return taskCollection.snapshots();
   }
 
+  Future<Response> addTodo(
+      {required String tarefa, required String descricao}) async {
+    Response response = Response();
+    DocumentReference documentReferencer = _collection.doc();
+
+    Task todo = Task(tarefa: tarefa, descricao: descricao);
+
+    Map<String, dynamic> data = todo.toJson();
+
+    await documentReferencer.set(data).whenComplete(() {
+      response.code = 200;
+      response.message = "Tarefa adcionada com sucesso";
+    }).catchError((e) {
+      response.code = 500;
+      response.message = e;
+    });
+
+    return response;
+  }
 
   Future<Response> deleteTodo({required String todoId}) async {
     Response response = Response();
@@ -22,9 +42,9 @@ class FirebaseCrud {
       response.code = 200;
       response.message = 'You deleted a task';
     }).catchError((e) {
-           response.code = 500;
-            response.message = e;
-        });
+      response.code = 500;
+      response.message = e;
+    });
     return response;
   }
 }
