5ae563af070e082f35e1fd418e869d3ff71f4af9
dev <adriana.bispo283@gmail.com>
2022-11-05 21:04:37 -0300

    criado metodo read no HomeController

--- lib/src/controller/home_controller.dart
+++ lib/src/controller/home_controller.dart
@@ -1,14 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../services/firebase_crud.dart';
 //models
 import '../models/response.dart';
-class HomeController{
+
+class HomeController {
   FirebaseCrud firebasecrud;
   HomeController({required this.firebasecrud});
-  
-  Future<Response>delete({required String todoId}) async{
 
+  Stream<QuerySnapshot> read() {
+    return firebasecrud.readTodo();
+  }
+
+  Future<Response> delete({required String todoId}) async {
     Response response = await firebasecrud.deleteTodo(todoId: todoId);
-    
+
     return response;
   }
 }
